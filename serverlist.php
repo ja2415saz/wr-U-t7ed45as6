47f9ad2a669f23c200667103fc81c45ca0a8077a
ja2415saz <113518940+ja2415saz@users.noreply.github.com>
2022-09-14 18:16:32 +0430

    Add files via upload

---
+++ serverlist.php
@@ -0,0 +1,11 @@
+
+<!-- saved from url=(0043)https://ja2415saz.github.io/wrUt-7ed45as6/serverlist.php -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>[{
+	"Name": "Rust133",
+	"Address": "185.231.114.99",
+	"Port": "28015"
+}, {
+	"Name": "Rust196",
+	"Address": "185.231.114.170",
+	"Port": "28015"
+} ]</body></html>
